f7c2a9bfa437b7965d00baeaa1f8eba467f579a6
hojun <hojun.sung@linecorp.com>
2022-02-13 23:07:19 +0900

    first commit

---
+++ src/main/java/com/boida/boidabackend/BoidaBackendApplication.java
@@ -0,0 +1,20 @@
+package com.boida.boidabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BoidaBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BoidaBackendApplication.class, args);
+    }
+
+    @GetMapping(path = "/api/hello/world")
+    public String helloWorld() {
+        return "Hello Best-House";
+    }
+}
---
+++ src/test/java/com/boida/boidabackend/BoidaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boida.boidabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoidaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
